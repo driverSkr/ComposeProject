b1943cabef7f43c465d2293804066d120fa6905e
zoucongbo <2237913536@qq.com>
2025-04-09 18:50:54 +0800

    播放gif图

--- app/src/main/java/com/ethan/compose/ui/component/page/ImagePage.kt
+++ app/src/main/java/com/ethan/compose/ui/component/page/ImagePage.kt
@@ -6,18 +6,28 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+import coil.decode.GifDecoder
+import coil.request.ImageRequest
+import coil.request.repeatCount
 import com.ethan.compose.R
 
 @Composable
@@ -26,7 +36,25 @@ import com.ethan.compose.R
  * Image 对应View中的 ImageView
  */
 fun ImagePage() {
-    Column(modifier = Modifier.fillMaxSize().padding(all = 10.dp)) {
+
+    val context = LocalContext.current
+
+    val bitmap1 = BitmapFactory.decodeResource(context.resources, R.mipmap.banner_01)
+    val imageRequest = remember {
+        ImageRequest.Builder(context)
+            .repeatCount(10)
+            .data("android.resource://${context.packageName}/${R.raw.app_logo_gif}")
+            .decoderFactory(GifDecoder.Factory())
+            .build()
+    }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(all = 10.dp)
+        .navigationBarsPadding()
+        .verticalScroll(rememberScrollState())
+    ) {
+
         Text(text = "基本使用")
         Image(painter = painterResource(id = R.mipmap.banner_01), contentDescription = "A woman")
 
@@ -37,12 +65,24 @@ fun ImagePage() {
 
         Spacer(modifier = Modifier.height(10.dp))
         Text(text = "传统的bitmap对象图片")
-        val context = LocalContext.current
-        val bitmap1 = BitmapFactory.decodeResource(context.resources, R.mipmap.banner_01)
         Image(bitmap = bitmap1.asImageBitmap(), contentDescription = "A woman")
 
         Spacer(modifier = Modifier.height(10.dp))
         Text(text = "使用网络图片")
         AsyncImage(model = "https://img-blog.csdnimg.cn/20200401094829557.jpg", contentDescription = "First line of code")
+
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "播放Gif动图")
+        SubcomposeAsyncImage(
+            model = imageRequest,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            loading = {
+                Image(painter = painterResource(R.mipmap.banner_01), contentDescription = null)
+            },
+            success = { success ->
+                SubcomposeAsyncImageContent(painter = success.painter, contentDescription = null)
+            }
+        )
     }
 }
